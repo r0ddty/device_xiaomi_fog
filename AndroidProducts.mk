c6191930e1c976201096cbf38ff9778814dc4683
alternoegraha <noegrahachan@gmail.com>
2024-03-21 14:00:07 +0700

    fog: update COMMON_LUNCH_CHOICES
    - A14 QPR2 change
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_fog-user \
-    aosp_fog-userdebug \
-    aosp_fog-eng
+    aosp_fog-ap1a-user \
+    aosp_fog-ap1a-userdebug \
+    aosp_fog-ap1a-eng
