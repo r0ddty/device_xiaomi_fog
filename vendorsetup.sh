fc246763ed1d61c3b4d8034b720463a2d5b77643
r0ddty <r0d@ukr.net>
2024-08-21 10:33:49 +0300

    Revert "fog: vendorsetup.sh: ship Luna kernel" It doesnt build and i am too lazy to check why, i am just gonna use WWY as usual
    This reverts commit ce11a50e3e393071445616851a463b10fb5a959c.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,8 +5,7 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone https://github.com/r0ddty/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-# git clone -b another-main https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
-git clone -b luna-staging https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 kernel/xiaomi/fog
+git clone -b another-main https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
 
 # Clone LOS hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
