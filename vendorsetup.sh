722a8be7fdcc6ac29e521771e5165ddc3f336e69
r0ddty <r0d@ukr.net>
2024-08-30 18:35:29 +0300

    fog: vendorsetup: fix OTA folder names
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,10 +15,10 @@ git clone -b leica-5.0 https://gitlab.com/r0ddty/vendor_xiaomi_miuicamera_fog ve
 
 # OTA
 rm -rf packages/apps/Updater
-git clone https://github.com/r0ddty/android_packages_apps_Updater_fog_unoff packages/apps/updater
+git clone https://github.com/r0ddty/android_packages_apps_Updater_fog_unoff packages/apps/Updater
 
 rm -rf vendor/RisingOTA
-git clone https://github.com/r0ddty/android_vendor_RisingOTA_fog_unoff vendor/RisingOTA
+git clone https://github.com/r0ddty/android_vendor_RisingOTA_fog_unoff vendor/risingOTA
 
 # Kernel fixups
 rm kernel/xiaomi/fog/Android.bp
