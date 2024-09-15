18fd1fd7df3cf1e93e5063d576e503c3fe8a7f30
r0ddty <r0d@ukr.net>
2024-09-15 15:04:09 +0300

    Revert "fog: use Alter's vendor tree" uh i just picked that one commit
    This reverts commit 31e976a57020a2c26bc5e5e8ec0a5bccd6b427b5.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone https://github.com/r0ddty/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
 git clone -b another-main https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
