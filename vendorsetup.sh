9f25d707953d513a7b327c8033094c256a3d0fa2
r0ddty <r0d@ukr.net>
2024-10-17 12:59:14 +0300

    fog: vendorsetup: change repos
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,10 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone -b fifteen https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b fifteen https://github.com/r0ddty/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone --depth=1 -b another-reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
+git clone --depth=1 -b another-main https://github.com/r0ddty/kernel_xiaomi_fog kernel/xiaomi/fog
 
 # Clone PixelOS-AOSP hardware/xiaomi
 git clone https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
@@ -14,7 +14,5 @@ git clone https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
 git clone https://github.com/pure-soul-kk/vendor_extra vendor/extra
 
 # Kernel fixups
-cd kernel/xiaomi/fog
-rm -rf Android.bp
-rm -rf techpack/audio/Android.bp
-cd ../../..
+rm kernel/xiaomi/fog/Android.bp
+rm kernel/xiaomi/fog/techpack/audio/Android.bp
