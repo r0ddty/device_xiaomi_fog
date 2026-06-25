6fd51c6e333efd552e163df9ebd8bf151f0f4e0f
r0ddty <r0d@ukr.net>
2026-06-25 17:38:24 +0300

    fog: fix my bad stuff
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ git clone -b awewa https://github.com/r0ddty/vendor_xiaomi_fog vendor/xiaomi/fog
 
 git clone -b motregen https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel --depth 1
 
-git clone -b motregen https://github.com/alternoegraha/kernel_xiaomi_sm6225 kernel/xiaomi/fog --depth 1
+#git clone -b motregen https://github.com/alternoegraha/kernel_xiaomi_sm6225 kernel/xiaomi/fog --depth 1
 
 git clone -b sixteen https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi --depth 1
 
