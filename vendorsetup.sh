e45dcb77b445bd0e3f8a02b16ff12e110e1ff2d9
r0ddty <r0d@ukr.net>
2026-09-09 10:19:24 +0300

    fog: derp it up
    Signed-off-by: r0ddty <r0d@ukr.net>

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+git clone -b awewa https://github.com/r0ddty/vendor_xiaomi_fog vendor/xiaomi/fog --depth 1
+
+git clone -b motregen https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel --depth 1
+
+#git clone -b motregen https://github.com/alternoegraha/kernel_xiaomi_sm6225 kernel/xiaomi/fog --depth 1
+
+git clone -b sixteen https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi --depth 1
+
+git clone -b lineage-24.0 https://github.com/LineageOS/android_device_lineage_sepolicy device/lineage/sepolicy --depth 1
+
+source ../rbe_d/setup.sh
